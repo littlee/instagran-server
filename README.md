5c7e0c4bdaf6c462c37783fea58cf5053c5d424c
littlee <kli@gizwits.com>
2017-05-03 00:32:29 +0800

    haha

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instagran Server
+
+微信小程序，Instagram 图片下载 服务端
---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var superagent = require('superagent')
+var cheerio = require('cheerio')
+var app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.send('hello')
+})
+
+app.post('/api/getimg', function(req, res) {
+  superagent
+    .post('http://www.dinsta.com/photos/')
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .send({
+      url: req.body.url
+    })
+    .end(function(err, resp) {
+      if (err) {
+        throw err
+      }
+
+
+      var $ = cheerio.load(resp.text)
+
+      res.json({
+        url: $('img').attr('src')
+      })
+    })
+})
+
+app.listen(80, function() {
+  console.log('server running on localhost:80')
+})
